d9459400ac38c5be49f7d329b217cf298e96af8a
iheathers <shresthapritam7@gmail.com>
2020-07-22 11:55:57 +0545

    Updates

---
+++ precache-manifest.74b7146a26c9b21dd494676026c1822b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66daa5031f758452d0238216a78890a8",
+    "url": "/Unsplash-search-images/index.html"
+  },
+  {
+    "revision": "f6d4371b076d0258a5f0",
+    "url": "/Unsplash-search-images/static/css/main.094c839f.chunk.css"
+  },
+  {
+    "revision": "270b1187a84c10e4e454",
+    "url": "/Unsplash-search-images/static/js/2.772622b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Unsplash-search-images/static/js/2.772622b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d4371b076d0258a5f0",
+    "url": "/Unsplash-search-images/static/js/main.4ec5719f.chunk.js"
+  },
+  {
+    "revision": "02bd315d9faec45d7cba",
+    "url": "/Unsplash-search-images/static/js/runtime-main.6096ab9a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Unsplash-search-images/precache-manifest.74b7146a26c9b21dd494676026c1822b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Unsplash-search-images/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
